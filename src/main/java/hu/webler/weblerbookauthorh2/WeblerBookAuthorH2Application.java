f37439ef2e335db59dcdb69efe7766949ea63438
Gabor <kapusgabor64@gmail.com>
2024-03-07 10:37:48 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerbookauthorh2/WeblerBookAuthorH2Application.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerbookauthorh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerBookAuthorH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerBookAuthorH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/hu/webler/weblerbookauthorh2/WeblerBookAuthorH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerbookauthorh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerBookAuthorH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
