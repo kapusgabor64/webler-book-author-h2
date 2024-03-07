56fffa402f923da27ade95a3f6256c7e111c3dea
Gabor <kapusgabor64@gmail.com>
2024-03-07 12:40:56 +0100

    create base models Author and Book

---
+++ src/main/java/hu/webler/weblerbookauthorh2/entity/Author.java
@@ -0,0 +1,46 @@
+package hu.webler.weblerbookauthorh2.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String name, List<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
---
+++ src/main/java/hu/webler/weblerbookauthorh2/entity/Book.java
@@ -0,0 +1,45 @@
+package hu.webler.weblerbookauthorh2.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(String title, Author author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
